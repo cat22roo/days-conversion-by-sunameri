ec85f0690ae8dca21fb6698dd732ef6ab11a849f
cat22roo <ctrltos@gmail.com>
2022-08-14 16:42:09 +0900

    コードを整理・ファビコン追加

--- days-conversion.js
+++ days-conversion.js
@@ -1,15 +1,15 @@
 'use strict';
 // 要素の取得
-const yourDate = document.getElementById("your-date");
+const yourDate = document.getElementById("your-date"); // 入力欄 1
+const includeOrNot = document.getElementById("form");// 選択肢
+const daysAfter = document.getElementById("days");// 入力欄 2
 const sunameriButton = document.getElementById("sunameri");
-const includeOrNot = document.getElementById("form");
-const daysAfter = document.getElementById("days");
 
 // ボタンを押したら
 sunameriButton.onclick = function () {
-  // 入力欄 1を取得「日付」
+  // 入力欄 1 の値を取得「日付」
   const inputYourDateValue = yourDate.value;
-  // 入力欄 2を取得「〇日後」
+  // 入力欄 2 の値を取得「〇日後」
   const inputDayAfter = daysAfter.value;
   const num = parseInt(inputDayAfter);
 
@@ -17,43 +17,16 @@ sunameriButton.onclick = function () {
   const date = new Date(inputYourDateValue);
   date.setDate(date.getDate() + num);
 
-  // 起点になる日の選択
+  // 減算（起点になる日の選択）
   let radioList = includeOrNot.choice;
   let include = radioList.value;
-  if (include === "include") { 
+  if (include === "include") {
     date.setDate(date.getDate() - 1);
-    } else {
-    // document.getElementById("choices").innerText = include;
-  }  
+  } else {
+  }
 
-// スナメリの回答
+  // スナメリの回答
   const answer = date.toLocaleDateString(); // 例)2017/5/18
   const output = `${inputDayAfter}日後は「${answer} 」です。`;
   document.getElementById("output-message").innerText = output;
 };
-
-
-/*
-let year = 2020.; 
-let month = 2;
-let day = 3;
-
-let dt = new Date("year/month/day");
-document.getElementById("startPoint").innerText = dt;
-
-/*
-function updateParagraph(){
-  let now = new Date();
-  let days = (now.getTime)
-}
-
-/*let today = new Date();
-let year = today.getFullYear();
-let month = today.getMonth() + 1;
-let date = today.getDate();
-let youbi = today.getDay();
-let youbis = new Array("日","月","火","水","木","金","土");
-let todayYoubi = youbis[ youbi ];
-let message = `今日は ${year} 年 ${month} 月 ${date} 日 (${todayYoubi}) です`;
-document.getElementById("today").innerHTML = message;
-*/
