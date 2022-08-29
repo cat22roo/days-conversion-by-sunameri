e135c88b0b1d73cba4c2dce052011982de453b92
cat22roo <ctrltos@gmail.com>
2022-08-29 17:49:48 +0900

    体裁を整える

--- days-conversion.js
+++ days-conversion.js
@@ -1,14 +1,14 @@
 'use strict';
 // 要素の取得
-const yourDate = document.getElementById("your-date"); // 入力欄 1
-const includeOrNot = document.getElementById("form"); // 選択肢
-const daysAfter = document.getElementById("days"); // 入力欄 2
-const sunameriButton = document.getElementById("sunameri"); // スナメリさんに聞くボタン
+const yourDate = document.getElementById("your-date"); // 入力欄 1（日付）
+const includeOrNot = document.getElementById("form"); // 選択肢（含める/含めない）
+const daysAfter = document.getElementById("days"); // 入力欄 2（〇日後）
+const sunameriButton = document.getElementById("sunameri"); // 「スナメリさんに聞く」ボタン
 const voiceArea = document.getElementById("output-message"); // 回答エリア
 const resetBtn = document.getElementById("reset"); // リセットボタン
-resetBtn.style.display = "none"; // デフォルトは非表示
+resetBtn.style.display = "none"; // デフォルトはリセットボタン非表示
 
-// ボタンを押したら
+// 「スナメリさんに聞く」ボタンを押したら
 sunameriButton.onclick = function () {
   // 入力欄 1 の値を取得「日付」
   const inputYourDateValue = yourDate.value;
@@ -20,15 +20,16 @@ sunameriButton.onclick = function () {
   const date = new Date(inputYourDateValue);
   date.setDate(date.getDate() + num);
 
-  // 減算（起点になる日の選択）
-  let radioList = includeOrNot.choice;
-  let include = radioList.value;
+  // 減算（起点になる日が含まれる場合）
+  const radioList = includeOrNot.choice;
+  const include = radioList.value;
   if (include === "include") {
     date.setDate(date.getDate() - 1);
   } else {
+      ;  // 何もしない
   }
-  
-  // 0 がキーボード入力できてしまうので、アラートを表示し処理中断
+
+  // 0 の場合は、アラート表示し処理を中断
   if (num === 0) {
     alert("0 は数える意味がありません");
     return;
@@ -53,7 +54,7 @@ sunameriButton.onclick = function () {
   // リセットボタンの処理
   resetBtn.onclick = function () {
     voiceArea.appendChild(header).remove();
-    voiceArea.classList.remove("voice");
-    resetBtn.style.display = "none";
+    voiceArea.classList.remove("voice"); // スナメリの回答を削除
+    resetBtn.style.display = "none"; // リセットボタン非表示
   };
 };
